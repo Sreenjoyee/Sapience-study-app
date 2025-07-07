09da5d417b6d689734f320bf28bfcbab8f1c4903
Sreenjoyee Saha <sreenjoyeesaha@gmail.com>
2025-07-08 01:47:30 +0530

    rendered some html files

--- run.py
+++ run.py
@@ -7,5 +7,22 @@ app = Flask(__name__)
 def home():
     return render_template('home_page.html')
 
+@app.route("/todo")
+def to_do_list():
+    return render_template('to_do_list.html')
+
+@app.route("/resources")
+def resources():
+    return render_template('resources.html')
+
+@app.route("/pomodromo")
+def pomodromo():
+    return render_template('pomodromo.html')
+
+@app.route("/calendar")
+def calendar():
+    return render_template('calendar.html')
+
+
 if __name__=='__main__':
     app.run(debug=True)
