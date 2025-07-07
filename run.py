acef04ddaac78881d84d8478e2e65ba04c085cb0
Sreenjoyee Saha <sreenjoyeesaha@gmail.com>
2025-07-08 01:27:44 +0530

    added route for home page, rendered the home html page and created a templates folder for html pages and static folder for css file

--- run.py
+++ run.py
@@ -3,8 +3,9 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def home():
+    return render_template('home_page.html')
 
 if __name__=='__main__':
     app.run(debug=True)
