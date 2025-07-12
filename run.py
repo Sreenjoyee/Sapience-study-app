e8f441c22432967faef3fca1c764ba808f052dc3
Sreenjoyee Saha <sreenjoyeesaha@gmail.com>
2025-07-12 22:03:30 +0530

    debug mode is off and gave felixibility for port as thisi is getting ready to be deployed on Render

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from studyapp import app
+from studyapp import app,os
+port = int(os.environ.get("PORT", 5000))
 
-if __name__=='__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port=port)
